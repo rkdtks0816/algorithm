da9c4b794ef1b290e607e7fcb6a32865fd15e11f
rkdtks0816 <rkdtks0816@naver.com>
2024-01-12 08:28:03 +0900

    solution : DFS2

--- algorithm/algorithm.cpp
+++ algorithm/algorithm.cpp
@@ -1,67 +1,72 @@
 ﻿/*
-1번 컴퓨터를 통해
-바이러스에 걸리게 되는
-컴퓨터의 수 출력
+모든 정점을 찍고
+다시 1번으로 돌아오기
+최소비용 출력
 */
 /*
-조건
-	컴퓨터의 수 100이하
-	컴퓨터 번호는 1번부터
+N : 1 ~ 12
+정점 사이로 이동할 수 없는 곳은 0으로 표기
+한번씩만 들를 수 있음
 */
 #include<iostream>
-#include<vector>
+
 using namespace std;
 
-/*
-init
-	컴퓨터의 수
-	컴퓨터 쌍의 수
-	연결된 컴퓨터 번호 쌍
-*/
-
 // input
-int N, M;
-vector<int> cpv[101]; // computer pair vector
+int N; // 정점 개수
+int costs[13][13]; // 정점사이 비용
 // variable
-int visited[101];
+int visited[13]; // 방문여부 저장
 // output
-int ans = 0;
+int ans = 2e9; // 최소 비용
 
+// 초기화
 void Init() {
-	cin >> N >> M;
-
-	// 입력값 노드별 벡터에 저장
-	for (int mi = 0; mi < M; mi++) {
-		int tmp_cp1, tmp_cp2;
-		cin >> tmp_cp1 >> tmp_cp2;
-		cpv[tmp_cp1].push_back(tmp_cp2);
-		cpv[tmp_cp2].push_back(tmp_cp1);
+	cin >> N;
+	for (int nr = 0; nr < N; nr++) {
+		for (int nc = 0; nc < N; nc++) {
+			cin >> costs[nr][nc];
+		}
 	}
+	// 1번 방문 표시
+	visited[0] = 1;
 }
 
-void DFS(int now) {
-	// 1번 부터 시작
-	for (int vi = 0; vi < cpv[now].size(); vi++) {
-		int next = cpv[now][vi];
-
-		// 방문한 노드 체크
+void DFS(int level, int now, int now_cost) {
+	// 모든 정점 -> 기저조건 level이 N까지
+	if (level == N - 1) {
+		if (costs[now][0] == 0) return;
+		// 다시 1번으로 돌아가야함
+		now_cost += costs[now][0];
+		// 최소값인지 확인
+		if (now_cost < ans) {
+			ans = now_cost;
+		}
+		return;
+	}
+	// 반복문
+	for (int ni = 0; ni < N; ni++) {
+		int next = ni;
+		// 갈 수 있는 정점 이동 costs[now][i] != 0;
+		if (costs[now][next] == 0) continue;
+		// 방문한 노드라면 continue;
 		if (visited[next] == 1) continue;
+		// 갈 수 있다면 방문 표시, 재귀
 		visited[next] = 1;
-		// 1번과 연결된 컴퓨터의 수 카운팅
-		ans++;
-		DFS(next);
-	}
+		DFS(level + 1, next, now_cost + costs[now][next]);
+		// 재귀 나와서 다시 방문 X 표시
+		visited[next] = 0;
+ 	}
 }
 
-// 컴퓨터의 수 출력
+// Output
 void Output() {
 	cout << ans;
 }
 
 int main() {
 	Init();
-	visited[1] = 1;
-	DFS(1);
+	DFS(0, 0, 0);
 	Output();
 	return 0;
 }
