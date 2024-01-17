57f56b8e8d583f7f3dde080921ba58a0766cb1ad
rkdtks0816 <rkdtks0816@naver.com>
2024-01-17 09:26:21 +0900

    01170926

--- algorithm/algorithm.cpp
+++ algorithm/algorithm.cpp
@@ -23,25 +23,35 @@ using namespace std;
 string ms; // magic str
 string ss[2]; // stone str
 // 변수
-
+int slen; // 돌다리 길이
 // 출력값
 int ans; // 돌다리를 건널 수 있는 방법의 수
 
 // 입력
 void Input() {
 	cin >> ms >> ss[0] >> ss[1];
+	slen = ss[0].size();
 }
 // 시뮬레이션
 // now : 뽑아야 하는 마법의 문자열 인덱스
-// now_str : 지금 뽑아야하는 문자열 0 : 악마, 1 : 천사, 2 : 둘 중 하나
-void Simulate(int now, int now_str) {
+// now_str : 지금 뽑아야하는 문자열 0 : 악마, 1 : 천사
+void Simulate(int now, int now_str, int start_idx) {
 	// 기저 조건 : 마법의 문자열 길이 만큼 뽑기
+	if (now == ms.size()) {
+		ans++;
+		return;
+	}
 	// 반복문
-	for (int si = 0; si < ss[0].size(); si++) {
+	for (int si = start_idx; si < slen; si++) {
 		// 마법의 문자열 뽑기
-		if (now_str == 2) {
-
-		}
+		if (ms[now] != ss[now_str][si]) continue;
+		// 뽑기
+		int next_str = now_str;
+		if (now_str == 0) next_str = 1;
+		else next_str = 0;
+		Simulate(now++, next_str, si++);
+		// 지나가기
+		Simulate(now, now_str, si++);
 	}
 }
 // 출력
@@ -51,7 +61,9 @@ void Output() {
 
 int main() {
 	Input();
-	Simulate(0, 2);
+	// 시작이 악마 : 0, 천사 : 1
+	Simulate(0, 0, 0);
+	Simulate(0, 1, 0);
 	Output();
 	return 0;
 }
