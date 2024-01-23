cf0750407800add474db9336b08dce5255f52f8b
송강산 <rkdtks0816@naver.com>
2024-01-24 08:47:38 +0900

    DFS7 : solve

--- algorithm/algorithm.cpp
+++ algorithm/algorithm.cpp
@@ -11,23 +11,54 @@
 	입력 앞의 학생이 더 잘헀다
 */
 #include<iostream>
+#include<vector>
 
 using namespace std;
 
 // 입력값
 int N, M, X; //학생수, 질문수, 타겟학생
 // 변수
-
+// 0: high, 1: low
+vector<int> position[2][100001];
+int visited[2][100001];
 // 출력값
 int U, V;
 
 // 입력
-void Init() {
+void init() {
 	cin >> N >> M >> X;
+	U = 1;
+	V = N;
 	for (int mi = 0; mi < M; mi++) {
-		int tmp
+		int tmpHigh, tmpLow;
+		cin >> tmpHigh >> tmpLow;
+		position[0][tmpLow].push_back(tmpHigh);
+		position[1][tmpHigh].push_back(tmpLow);
 	}
 }
 // 시뮬
-// 타겟 학생을 중심으로 큰지 작은지 확인
-// 
+// 타겟 학생을 시작
+void runSimulation(int now, int nowPosition) {
+	// 타고타고 들어가기
+	for (int i = 0; i < position[nowPosition][now].size(); i++) {
+		int next = position[nowPosition][now][i];
+		
+		if (visited[nowPosition][next] == 1) continue;
+		visited[nowPosition][next] = 1;
+		if (nowPosition == 0) U++;
+		else V--;
+		runSimulation(next, nowPosition);
+	}
+}
+void output() {
+	cout << U << endl << V;
+}
+
+int main() {
+	init();
+	runSimulation(X, 0);
+	runSimulation(X, 1);
+	output();
+
+	return 0;
+}
